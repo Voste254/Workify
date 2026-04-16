4711d055ba7f7c45f9fc89c41ea988dbab0c1062
Voste254 <onyangosteveok@gmail.com>
2026-04-16 16:15:13 +0300

    Add DM Sans and DM Mono font families to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', 'sans-serif'],
+        mono: ['"DM Mono"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
